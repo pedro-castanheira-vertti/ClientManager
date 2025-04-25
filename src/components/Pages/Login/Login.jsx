386ae59d283d173b9f52e0589be385ad85dc692f
Pedro Castanheira <estagiario3@vertti.com.br>
2025-04-25 11:07:02 -0300

    Criação do componente login e registerLogin

---
+++ src/components/Pages/Login/Login.jsx
@@ -0,0 +1,54 @@
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import InputField from '../../GlobalComponents/InputField/InputField';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate()
+
+    async function handleLogin(e) {
+        e.preventDefault()
+
+        try {
+            const response = await axios.post('http://localhost:8080/login', {
+                email,
+                password,
+            })
+
+            const token = response.data.token
+            localStorage.setItem('token', token)
+            navigate('/home')
+        } catch (error) {
+            console.error('Erro ao fazer login:', error)
+        }
+    }
+
+    const inputFields = [
+        { placeholder: 'Email', type: 'email', name: 'email', value: email, onChange: (e) => setEmail(e.target.value) },
+        { placeholder: 'Senha', type: 'password', name: 'senha', value: password, onChange: (e) => setPassword(e.target.value) }
+    ]
+
+    return (
+        <div className='container-login'>
+            <h1>Login</h1>
+            <form onSubmit={handleLogin}>
+                <div className='form-content'>
+                    <InputField inputFields={inputFields} />
+                    <button type='submit' className='enter'>
+                        Entrar
+                    </button>
+                </div>
+            </form>
+            <div className='rodape'>
+                <p>Ainda não tem registro?</p>
+                <Link to='/registerCorretor' className='register'>
+                    Registre-se aqui
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/Pages/RegisterLogin/RegisterLogin.jsx
@@ -0,0 +1,66 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import api from '../../../services/api';
+import InputField from '../../GlobalComponents/InputField/InputField';
+import './styleRegisterCorretor.css'
+
+function RegisterLogin() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        cpf: '',
+        password: ''
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
+    async function createCorretor(event) {
+        event.preventDefault()
+        try {
+            await api.post('/corretor/create', {
+                name: formData.name,
+                email: formData.email,
+                cpf: formData.cpf,
+                password: formData.password
+            })
+            alert('Corretor cadastrado com sucesso!')
+            navigate('/');
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    const inputFields = [
+        { placeholder: 'Nome completo', type: 'text', name: 'name', value: formData.name, onChange: handleChange },
+        { placeholder: 'Cpf', type: 'number', name: 'cpf', value: formData.cpf, onChange: handleChange },
+        { placeholder: 'Email', type: 'email', name: 'email', value: formData.email, onChange: handleChange },
+        { placeholder: 'Senha', type: 'password', name: 'password', value: formData.password, onChange: handleChange }
+    ]
+
+    return (
+        <div className='container-register'>
+            <h1>Registre-se</h1>
+            <form onSubmit={createCorretor}>
+                <div className='form-content'>
+                    <InputField inputFields={inputFields} />
+                    <button type='submit' className='enter' >
+                        Entrar
+                    </button>
+                </div>
+                <div className='rodape'>
+                    <p>Já possui cadastro?</p>
+                    <Link to='/' className='login'>
+                        Entrar
+                    </Link>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterLogin
