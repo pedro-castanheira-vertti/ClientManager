0b791e5591c91909925c00c655210cb221e9ac80
Pedro Castanheira <estagiario3@vertti.com.br>
2025-04-25 10:52:02 -0300

    Routes manager (server.js) config

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import cors from 'cors';
-import {bcrypt} from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import userRouter from './routes/userRoutes.js';
+import corretorRouter from './routes/corretorRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/users', userRouter);
+app.use('/corretor', corretorRouter);
+
+app.listen(8080);
+
 
